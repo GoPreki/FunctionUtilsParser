0a04d0a572374f186d5f682400e671183fa2d0fd
Mariana Rodríguez <m.rodriguez21@uniandes.edu.co>
2020-07-17 19:05:36 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(
+    name="preki_funcparser",
+    version="1.0.0",
+    author="Preki",
+    author_email="mariana@gopreki.com",
+    packages=['preki_funcparser'],
+    url='https://gopreki.com',
+    download_url="https://github.com/GoPreki/FunctionUtilsParser",
+    license="MIT",
+    description="Python library for Preki Functions usage with Parsers",
+    long_description="Python library for Preki Functions usage with Parsers",
+    install_requires=[],
+)
