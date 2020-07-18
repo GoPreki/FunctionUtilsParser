2333381e9756b882b81bf556875bf10449710d32
Mariana Rodríguez <m.rodriguez21@uniandes.edu.co>
2020-07-17 19:06:25 -0500

    Add decimal parsers to package

---
+++ preki_funcparser/parsers.py
@@ -0,0 +1,21 @@
+import json
+from decimal import Decimal
+
+
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, Decimal):
+            return float(o) if o % 1 > 0 else int(o)
+        return super(DecimalEncoder, self).default(o)
+
+
+class Parser:
+    def __init__(self, data):
+        self.data = data
+
+    def to_number(self):
+        return json.loads(
+            json.dumps(self.data, cls=DecimalEncoder))
+
+    def to_decimal(self):
+        return json.loads(json.dumps(self.data), parse_float=Decimal)
