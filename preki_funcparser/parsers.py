a0c4f69de70645272246b6437522faff104e2491
Mariana Rodríguez <m.rodriguez21@uniandes.edu.co>
2022-04-25 18:41:03 -0500

    fix[PAR]: Solve truncation issue for negative decimal numbers

--- preki_funcparser/parsers.py
+++ preki_funcparser/parsers.py
@@ -5,7 +5,7 @@ from decimal import Decimal
 class DecimalEncoder(json.JSONEncoder):
     def default(self, o):
         if isinstance(o, Decimal):
-            return float(o) if o % 1 > 0 else int(o)
+            return float(o) if o % 1 != 0 else int(o)
         return super(DecimalEncoder, self).default(o)
 
 
